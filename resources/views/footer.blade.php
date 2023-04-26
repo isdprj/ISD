7ae0bc51f1650bbd5e3b2f5de81fd35e6fda4edf
hongngoc00 <128308233+isdprj@users.noreply.github.com>
2023-04-26 22:04:13 +0700

    contact_footer update

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,9 +1,9 @@
 <div id="footer" class="color-div">
     <div class="container">
         <div class="row">
-            <div class="col-sm-2">
+            <div class="col-sm-3">
                 <div class="widget">
-                    <h4 class="widget-title">Social</h4>
+                    <h4 class="widget-title">Cộng đồng</h4>
                     <div id="social-lists"><div>
                         <a href="#"><i class="social-size ti-facebook"></i></a>
                         <a href="#"><i class="social-size ti-instagram"></i> </a>
@@ -13,34 +13,32 @@
                 </div>
                 </div>
             </div>
-            <div class="col-sm-3">
+            <div class="col-sm-4">
                  <div class="widget"> 
-                    <h4 class="widget-title">Location</h4>
+                    <h4 class="widget-title">Địa chỉ</h4>
                      <div>
                         <div class="map-sizing wow flipInX"><iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3678.0141923553406!2d89.551518!3d22.801938!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39ff8ff8ef7ea2b7%3A0x1f1e9fc1cf4bd626!2sPranon+Pvt.+Limited!5e0!3m2!1sen!2s!4v1407828576904" ></iframe></div>
                     </div>                    
                 </div>
             </div>
-            <div class="col-sm-4">
+            <div class="col-sm-3">
              <div class="col-sm-10">
                 <div class="widget">
-                    <h4 class="widget-title">Contact Us</h4>
+                    <h4 class="widget-title">Liên hệ</h4>
                     <div>
-                        <div class="contact-info">
-                            <i class="fa fa-map-marker"></i>
-                            <p>30 South Park Avenue San Francisco, CA 94108 Phone: +78 123 456 78</p>
-                            <p>Nemo enim ipsam voluptatem quia voluptas sit asnatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione.</p>
-                        </div>
+                            <p>Địa chỉ: Khu đô thị Mễ Trì Hạ, Mễ Trì, Nam Từ Liêm, Hà Nội </p>
+                            <p>Phone: +84 328 047 403</p>
+                            <p>Email: ahksport@gmail.com</p>
                     </div>
                 </div>
               </div>
             </div>
-            <div class="col-sm-3">
+            <div class="col-sm-2">
                 <div class="widget">
-                    <h4 class="widget-title">Newsletter Subscribe</h4>
+                    <h4 class="widget-title">Cập nhật tin tức</h4>
                     <form action="#" method="post">
-                        <input type="email" name="your_email">
-                        <button class="pull-right" type="submit">Subscribe <i class="fa fa-chevron-right"></i></button>
+                        <input type="email" name="your_email" placeholder="Email">
+                        <button class="pull-right" type="submit">Đăng ký<i class="fa fa-chevron-right"></i></button>
                     </form>
                 </div>
             </div>
